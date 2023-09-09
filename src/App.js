512d26f03d6334e231e02a410681bd9874f09dc3
Kamran012 <125637283+Kamran012@users.noreply.github.com>
2023-09-09 15:22:22 +0530

    topcourses

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { filterData, apiUrl } from "./data";
+import Navbar from "./Components/Navbar";
+import Filter from "./Components/Filter";
+import Cards from "./Components/Cards";
+import Spinner from "./Components/Spinner";
+import { toast } from "react-toastify"
 
-function App() {
+
+const App = () => {
+  const [courses, setCourses] = useState([]);
+  const [category, setCategory] = useState("All");
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(apiUrl);
+      const output = await response.json();
+      // console.log(output.data);
+      setCourses(output.data);
+    }
+    catch (err) {
+      // console.log(err);
+      toast.error("Something Went Wrong");
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col ">
+      <Navbar />
+      <div
+        className="bg-bgDark2 min-h-screen">
+        <Filter filterData={filterData} category={category} setCategory={setCategory} />
+
+        <div className="w-11/12 max-w-[1200px]  mx-auto flex flex-wrap items-center justify-center">
+          {
+            loading ? <Spinner /> : <Cards courses={courses} category={category} />
+          }
+        </div>
+      </div>
     </div>
-  );
-}
+  )
+};
 
 export default App;
